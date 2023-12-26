7a81617cb253000bcaddcdce0b93084964b3bfc4
rc-balaji <balajic@drngpit.ac.in>
2023-12-26 15:45:18 +0530

    first commit

---
+++ J1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Sam{
+	public static void main(String args[]){
+		
+		static Scanner in = new Scanner(System.in);
+		
+		int a=10, b=30;
+		int c=a+b;
+		System.out.println("Result : "+c);
+	}
+}
+
+class Sub{
+	public static void main(String args[]){
+		System.out.println("Hello Wolrd");
+	}
+}
