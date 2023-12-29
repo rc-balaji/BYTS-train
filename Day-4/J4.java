4e08f1d00c34b5dd3f100eada5a3395a4b2a3350
rc-balaji <balajic@drngpit.ac.in>
2023-12-29 10:10:51 +0530

    sss

---
+++ Day-4/J4.java
@@ -0,0 +1,21 @@
+/**
+ * J4
+ */
+public class J4 {
+
+    public static void main(String[] args){
+
+        String str = "Hello World";
+
+        for(int i=0;i<str.length();i++){
+
+            if(str.charAt(i) != ' '){
+
+               
+
+            }
+
+        }
+
+    }
+}
